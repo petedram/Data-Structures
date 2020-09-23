68136ce22e606b89f950c749016b30fa9feb27b2
PeteDram <me@petejames.com>
2020-09-22 20:28:35 -0700

    part 1 of BST complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,4 @@
+
 """
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
@@ -17,20 +18,65 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        def fn(value, curr):
+            if value >= curr.value: #if same value, go right per test?
+                #if right child is None
+                if curr.right is None:
+                    curr.right = BSTNode(value)
+                    return
+                else:
+                    fn(value, curr.right)
+            elif value < curr.value:
+                # if left child is None
+                if curr.left is None:
+                    curr.left = BSTNode(value)
+                    return
+                else:
+                    fn(value, curr.left)
+        fn(value, self) # recursive call
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        def fn(value, curr):
+            #check if self.value is target
+            if value == curr.value:
+                #if yes, return True 
+                return True
+            elif value > curr.value:
+                # go right
+                if curr.right is None:
+                    return False
+                else:
+                    return fn(value, curr.right)
+            elif value < curr.value:
+                # go left
+                if curr.left is None:
+                    return False
+                else:
+                    return fn(value, curr.left)
+        return fn(target, self)        
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        #go right until you can't go right any more
+        def fn(curr):
+            if curr.right is None:
+                return curr.value
+            return fn(curr.right)
+        return fn(self)
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        #one side then the other
+        def fnt(curr):
+            fn(curr.value)
+            if curr.left:
+                fnt(curr.left)
+            if curr.right:
+                fnt(curr.right)
+        fnt(self)
+
 
     # Part 2 -----------------------
 
@@ -80,6 +126,6 @@ print("elegant methods")
 print("pre order")
 bst.pre_order_dft()
 print("in order")
-bst.in_order_dft()
+bst.in_order_print() # bst.in_order_dft()
 print("post order")
 bst.post_order_dft()  
